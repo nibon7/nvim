0d0897da57e2501d3a763f5cc6b5c44cc5346663
nibon7 <nibon7@163.com>
2025-09-23 20:40:26 +0800

    feat(ui): Set default border style

--- init.lua
+++ init.lua
@@ -164,6 +164,9 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Set default border style
+vim.o.winborder = 'rounded'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
