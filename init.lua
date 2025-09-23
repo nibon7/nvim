62dbe6dffe03859aaf80eaec601d9d4a1da7acba
nibon7 <nibon7@163.com>
2025-09-23 20:40:26 +0800

    feat(ui): Set default border style

--- init.lua
+++ init.lua
@@ -171,6 +171,9 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- Set default border style
+  vim.o.winborder = 'rounded'
 end
 
 -- ============================================================
