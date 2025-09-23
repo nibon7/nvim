a0d92becd8b388e9edb635d146faf7aac5bcbf64
nibon7 <nibon7@163.com>
2025-09-23 13:04:17 +0800

    feat(blink.cmp): Map <CR> to accept completion item

--- init.lua
+++ init.lua
@@ -919,6 +919,9 @@ do
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
       preset = 'default',
 
+      -- Accept the currently selected item
+      ['<CR>'] = { 'select_and_accept', 'fallback' },
+
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
