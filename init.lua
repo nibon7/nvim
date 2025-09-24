5b25052a48114001113c28cdc0a6e1f94629fc89
nibon7 <nibon7@163.com>
2025-09-24 11:11:05 +0800

    feat(ui): Prefer the tokyonight-moon theme

--- init.lua
+++ init.lua
@@ -437,7 +437,7 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'tokyonight-moon'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
