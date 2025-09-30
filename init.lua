4d66588dcb6d9da5a15c87d07bc3455ffad94b1a
nibon7 <nibon7@163.com>
2025-09-30 22:46:55 +0800

    feat(blink.cmp): Add buffer to completion source

--- init.lua
+++ init.lua
@@ -819,7 +819,7 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets' },
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
 
       snippets = { preset = 'luasnip' },
