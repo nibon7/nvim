f2e49db064de9f6271183f1bebed9d31c2908da3
nibon7 <nibon7@163.com>
2025-12-16 23:10:47 +0800

    feat(lsp): Enable bash language server

--- init.lua
+++ init.lua
@@ -750,6 +750,7 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
+    bashls = {},
     clangd = {},
     -- gopls = {},
     pyright = {},
