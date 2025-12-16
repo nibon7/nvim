637e9776539f1e747c562ef876c561be230a1e54
nibon7 <nibon7@163.com>
2025-12-16 23:12:28 +0800

    feat(format): Enable typst formatter

--- init.lua
+++ init.lua
@@ -877,6 +877,7 @@ do
     -- You can also specify external formatters in here.
     formatters_by_ft = {
       -- rust = { 'rustfmt' },
+      typst = { 'typstyle' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
