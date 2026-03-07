0d6295d01e0f771f33273b4a02b0e02693367929
nibon7 <nibon7@163.com>
2026-03-07 22:05:39 +0800

    feat: Add render-markdown

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+-- Plugin to improve viewing Markdown files in Neovim
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+
+vim.pack.add {
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-mini/mini.nvim',
+}
