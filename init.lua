faabcd17f341c22eb9fb44e7fad5970ad182dcc3
nibon7 <nibon7@163.com>
2026-05-28 22:41:13 +0800

    feat(format): Enable python formatter

--- init.lua
+++ init.lua
@@ -893,7 +893,7 @@ do
       -- rust = { 'rustfmt' },
       typst = { 'typstyle' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { 'isort', 'black' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
