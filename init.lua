2a7c4f8bd31056225e7af48af173f20986eb31fa
nibon7 <nibon7@163.com>
2026-06-26 09:35:30 +0800

    feat(format): Enable tex-fmt formatter

--- init.lua
+++ init.lua
@@ -897,6 +897,7 @@ do
       typst = { 'typstyle' },
       -- Conform can also run multiple formatters sequentially
       python = { 'isort', 'black' },
+      tex = { 'tex-fmt' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
